175f0269b15acd3e3e3cff8927203761bf4f7b27
Minh Hieu <hieuami@gmail.com>
2021-12-07 12:13:43 +0700

    lifecycle

--- src/components/TextBox/TextBox.tsx
+++ src/components/TextBox/TextBox.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TextBoxProps } from "./TextBox.model";
 import style from "./TextBox.module.sass";
 
 export default function TextBox(props: TextBoxProps) {
-  const [text, setText] = useState(props.value);
+  const [text, setText] = useState("");
   const [txtCls, setTxtCls] = useState(style.txt_as_label);
   const [focus, setFocus] = useState(false);
 
-  // useEffect(() => {
-  //   setText(props.value);
-  // }, [props.value]);
+  useEffect(() => {
+    setText(props.value);
+  }, []);
 
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
     setText(e.currentTarget.value);
